88816da7471f71d1980027b4d0af063e97dfede9
Shivam Kasaudhan <kshivamit@gmail.com>
2026-02-01 18:02:15 +0530

    Add product service with few feature GetAll and create.

---
+++ RetailShop.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using RetailShop.Application.DTOs.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailShop.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<Guid> CreateAsync(ProductCreateDto dto);
+        Task<ProductResponseDto> GetByIdAsync(Guid id);
+        Task<IEnumerable<ProductResponseDto>> GetAllAsync();
+        Task UpdateAsync(Guid id, ProductCreateDto dto);
+        Task DeleteAsync(Guid id);
+
+    }
+}
---
+++ RetailShop.Infrastructure/Services/ProductService.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailShop.Application.DTOs.Product;
+using RetailShop.Application.Interfaces;
+using RetailShop.Domain.Entities;
+using RetailShop.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailShop.Infrastructure.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly RetailShopDbContext _context;
+
+        public ProductService(RetailShopDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Guid> CreateAsync(ProductCreateDto dto)
+        {
+            var product = new Product
+            { 
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+                CategoryId = dto.CategoryId,
+                Inventory = new Inventory
+                {
+                    Quantity = dto.InitialStock
+                }
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync(); 
+            return product.Id;
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductResponseDto>> GetAllAsync()
+        {
+            return await _context.Products.Include(p => p.Category).Include(p => p.Inventory)
+                .Select(p => new ProductResponseDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    CategoryName = p.Category.Name,
+                    AvailableStock = p.Inventory.Quantity
+                }).ToListAsync();
+        }
+
+        public Task<ProductResponseDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Guid id, ProductCreateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
